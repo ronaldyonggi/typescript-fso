2e72e0f6e8a164c74c89dac1d10be3dc33799007
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-12-18 16:22:52 +0800

    9.5 endpoint added for bmi calculator

--- index.ts
+++ index.ts
@@ -1,8 +1,28 @@
 import express from 'express'
+import { calculateBmi } from './bmiCalculator'
+
 const app = express()
 
 app.get('/hello', (_req, res) => {
     res.send('Hello Full Stack!')
+})
+
+app.get('/bmi', (req, res) => {
+    const queryHeight = req.query.height
+    const queryWeight = req.query.weight
+
+    if (!queryHeight || !queryWeight) {
+        res.status(400).json({error: "missing parameters"})
+    }else if (isNaN(Number(queryHeight)) || isNaN(Number(queryWeight))) {
+        res.status(400).json({ error: "malformatted parameters"})
+    } else {
+        const height = Number(queryHeight)
+        const weight = Number(queryWeight)
+        const bmi = calculateBmi(height, weight)
+        res.json({
+            height, weight, bmi
+        })
+    }
 })
 
 const PORT = 3003;
