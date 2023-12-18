ac837a9d7c4c26e926f9ec6cfba780ae155cb544
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-12-18 14:03:04 +0800

    9.3 implement parseExerciseArguments parses args for calculateExercises

--- utils.ts
+++ utils.ts
@@ -3,6 +3,11 @@ interface BmiParameters {
     weight: number
 }
 
+interface ExerciseParameter {
+    target: number,
+    hours: number[]
+}
+
 export const parseBmiArguments = (args: string[]): BmiParameters => {
     if (args.length < 4) throw new Error('Not enough arguments')
     if (args.length > 4) throw new Error('Too many arguments')
@@ -15,4 +20,31 @@ export const parseBmiArguments = (args: string[]): BmiParameters => {
     } else {
         throw new Error('Provided value were not numbers!')
     }
+}
+
+export const parseExerciseArguments = (args: string[]): ExerciseParameter => {
+    if (args.length < 4) throw new Error('Not enough arguments')
+
+    let target
+    // Check for target argument
+    if (isNaN(Number(args[2]))) {
+        throw new Error('Provided target is not a number!')
+    } else {
+        target = Number(args[2])
+    }
+
+    const hours: number[] = []
+    // Iterate arguments starting 
+    for (let i = 3; i < args.length; i++) {
+        if (isNaN(Number(args[i]))) {
+            throw new Error('One of the period length is not a number!')
+        } else {
+            hours.push(Number(args[i]))
+        }
+    }
+
+    return {
+        target, hours
+    }
+
 }
